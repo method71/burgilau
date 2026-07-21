dd1c8bed280e90f12748d03d5474b0b127c04f6f
method71 <vnevremen420@gmail.com>
2026-07-21 23:21:07 +0500

    Add Sharp image optimization dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   base: "/burgilau/",
+  plugins: [
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|webp)$/i,
+      png: {
+        compressionLevel: 9,
+        quality: 82,
+      },
+      jpeg: {
+        mozjpeg: true,
+        quality: 82,
+      },
+      jpg: {
+        mozjpeg: true,
+        quality: 82,
+      },
+      webp: {
+        effort: 6,
+        quality: 82,
+      },
+    }),
+  ],
 });
